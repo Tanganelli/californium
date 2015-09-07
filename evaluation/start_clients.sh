547caceeacb4b12ef68ade8a51e5fea5e21fd119
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-09-07 11:49:05 +0200

    start_clients.sh

--- evaluation/start_clients.sh
+++ evaluation/start_clients.sh
@@ -8,5 +8,5 @@ tLen=${#pmins[@]}
 for (( i=0; i<${tLen}; i++ ));
 do
     echo "Client $i - pmin ${pmins[$i]} -pmax ${pmaxs[$i]}"
-java -jar ../run/cf-coreinterface-client-1.0.0-SNAPSHOT.jar -l result$i.log -n $2 -pmax ${pmaxs[$i]} -pmin ${pmins[$i]} -u coap://127.0.0.1:5683/127.0.0.2:5683/CoREInterfaceResource -i 10.0.0.5 &
+java -jar ../run/cf-coreinterface-client-1.0.0-SNAPSHOT.jar -l result$i.log -n $1 -pmax ${pmaxs[$i]} -pmin ${pmins[$i]} -u coap://127.0.0.1:5683/127.0.0.2:5683/CoREInterfaceResource -i 10.0.0.5 &
 done
