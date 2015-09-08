a69e760c217e737a76fc32a8d4bcc81d51dc74fe
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-09-08 15:07:55 +0200

    clients on localhost

--- evaluation/start_clients.sh
+++ evaluation/start_clients.sh
@@ -8,7 +8,7 @@ tLen=${#pmins[@]}
 for (( i=0; i<${tLen}; i++ ));
 do
     echo "Client $i - pmin ${pmins[$i]} -pmax ${pmaxs[$i]}"
-	java -jar ../run/cf-coreinterface-client-1.0.0-SNAPSHOT.jar -l result$i.log -n $1 -pmax ${pmaxs[$i]} -pmin ${pmins[$i]} -u coap://10.0.0.1:5683/10.0.0.2:5683/CoREInterfaceResource -i 10.0.0.4 > out$i &
+	java -jar ../run/cf-coreinterface-client-1.0.0-SNAPSHOT.jar -l result$i.log -n $1 -pmax ${pmaxs[$i]} -pmin ${pmins[$i]} -u coap://10.0.0.1:5683/10.0.0.2:5683/CoREInterfaceResource -i 127.0.0.1 > out$i &
 done
 #sleep 2m
 #echo "Add delay 1 sec"
