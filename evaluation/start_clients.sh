3ef50f5e93152fcc28e00f46706f1df655d5e3f0
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-09-08 13:01:49 +0200

    start_clients

--- evaluation/start_clients.sh
+++ evaluation/start_clients.sh
@@ -10,7 +10,7 @@ do
     echo "Client $i - pmin ${pmins[$i]} -pmax ${pmaxs[$i]}"
 	java -jar ../run/cf-coreinterface-client-1.0.0-SNAPSHOT.jar -l result$i.log -n $1 -pmax ${pmaxs[$i]} -pmin ${pmins[$i]} -u coap://127.0.0.1:5683/10.0.0.2:5683/CoREInterfaceResource -i 127.0.0.1 > out$i &
 done
-sleep 2m
-echo "Add delay 1 sec"
-ipfw add pipe 1 in proto udp
-ipfw pipe 1 config delay 1000ms
+#sleep 2m
+#echo "Add delay 1 sec"
+#ipfw add pipe 1 in proto udp
+#ipfw pipe 1 config delay 1000ms
