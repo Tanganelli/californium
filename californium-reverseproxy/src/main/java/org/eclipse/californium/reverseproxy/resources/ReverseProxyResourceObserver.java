d8125b2ac3de20ee799f54771078edf11c78ec40
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-09-10 10:18:52 +0200

    logging

--- californium-reverseproxy/src/main/java/org/eclipse/californium/reverseproxy/resources/ReverseProxyResourceObserver.java
+++ californium-reverseproxy/src/main/java/org/eclipse/californium/reverseproxy/resources/ReverseProxyResourceObserver.java
@@ -1,20 +1,21 @@
 package org.eclipse.californium.reverseproxy.resources;
 
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.eclipse.californium.core.coap.Request;
-import org.eclipse.californium.core.network.Exchange;
-import org.eclipse.californium.core.network.RemoteEndpoint;
 import org.eclipse.californium.core.observe.ObserveRelation;
 import org.eclipse.californium.core.server.resources.Resource;
 import org.eclipse.californium.core.server.resources.ResourceObserver;
-import org.eclipse.californium.reverseproxy.PeriodicRequest;
 
 /**
  * Used to react to changes performed by the client on the resource.
  */
 public class ReverseProxyResourceObserver implements ResourceObserver{
 
+	/** The logger. */
+	protected final static Logger LOGGER = Logger.getLogger(ReverseProxyResourceObserver.class.getCanonicalName());
+	
 	private ReverseProxyResource ownerResource;
 
 	public ReverseProxyResourceObserver(ReverseProxyResource ownerResource){
@@ -53,6 +54,7 @@ public class ReverseProxyResourceObserver implements ResourceObserver{
 
 	@Override
 	public void removedObserveRelation(ObserveRelation relation) {
+		LOGGER.log(Level.INFO, "removedObserveRelation(" + relation + ")");
 		Request req = relation.getExchange().getRequest();
 		ClientEndpoint clientEndpoint = new ClientEndpoint(req.getSource(), req.getSourcePort());
 		ownerResource.deleteSubscriptionsFromClients(clientEndpoint);
